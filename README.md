e971573f04f84ece9597ff4915f067db314fd427
max <max.ivanov@do-it.co>
2017-03-06 19:23:58 +0200

    Store profile data, Product CRUD

---
+++ README.md
@@ -0,0 +1,1 @@
+NodeJS wrapper for ecwid.com API
---
+++ ecwid.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var request = require('request-promise');
+var qs = require('qs');
+
+var STORE_ID;
+var ACCESS_TOKEN;
+var API_VERSION = 'v3';
+var BASE_URL = 'https://app.ecwid.com/api/' + API_VERSION + '/';
+var PATH = {
+  profile: 'profile',
+  products: 'products'
+};
+
+function ecwid(storeId, accessToken) {
+  if(!storeId) throw new Error('StoreID is required');
+  if(!accessToken) throw new Error('Access token is required');
+  STORE_ID = storeId;
+  ACCESS_TOKEN = accessToken;
+
+  return {
+    getStoreProfile: getStoreProfile,
+    getProducts: getProducts,
+    addProduct: addProduct,
+    deleteProduct: deleteProduct,
+    updateProudct: updateProudct
+  }
+}
+
+function getStoreProfile() {
+  return exec(PATH.profile, 'get')
+}
+
+function getProducts() {
+  return exec(PATH.products, 'get')
+}
+
+function addProduct(product) {
+  return exec(PATH.products, 'post', product)
+}
+
+function deleteProduct(productId) {
+  return exec(PATH.products + '/' + productId, 'delete')
+}
+
+function updateProudct(productId, product) {
+  return exec(PATH.products + '/' + productId, 'put', product)
+}
+
+function exec(path, method, data) {
+  var uri = BASE_URL + STORE_ID + '/' + path + '?' + qs.stringify({token: ACCESS_TOKEN});
+
+  var options = { uri: uri };
+
+  if(data) options.body = JSON.stringify(data);
+
+  return request[method](options).then(function(data) {
+    return JSON.parse(data)
+  })
+}
+
+module.exports = ecwid;
